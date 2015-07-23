2f38f8acc13d1673b6e0f7d60919c35fb28d26ea
Hao Hu <hhu@thoughtworks.com>
2015-07-23 22:21:46 +0800

    add pos.scan() method

--- pos_v2/main/model/pos.js
+++ pos_v2/main/model/pos.js
@@ -2,3 +2,10 @@ function Pos(scanner, cart) {
   this.scanner = scanner;
   this.cart = cart;
 }
+
+Pos.prototype.scan = function(tag) {
+  var cartItem = this.scanner.scan(tag);
+  if (cartItem) {
+    this.cart.addCartItem(cartItem);
+  }
+};
