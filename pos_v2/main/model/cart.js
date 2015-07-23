2fba07325d9f46708db013add6fd1062a5cf7f65
Hao Hu <hhu@thoughtworks.com>
2015-07-23 22:48:06 +0800

    add cart.addCartItem() method

--- pos_v2/main/model/cart.js
+++ pos_v2/main/model/cart.js
@@ -2,6 +2,15 @@ function Cart() {
   this.cartItems = [];
 }
 
+Cart.prototype.addCartItem = function(cartItem) {
+  var existedCartItem = this.findCartItem(cartItem.getBarcode());
+  if (existedCartItem) {
+    existedCartItem.count += cartItem.count;
+  } else {
+    this.cartItems.add(cartItem);
+  }
+};
+
 Cart.prototype.findCartItem = function(barcode) {
   for (var i = 0; i < this.cartItems.length; i++) {
     if (this.cartItems[i].getBarcode() === barcode) {
